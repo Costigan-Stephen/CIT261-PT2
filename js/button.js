81e9f9990a0b770a5c710e06db3b421444d38687
Costigan-Stephen <stephenjcostigan@hotmail.com>
2020-02-19 23:36:29 -0600

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,126 @@
+window.onbeforeunload = function () {
+    reset();
+}
+
+function clicked(element) {
+
+    var x = document.getElementById("x").value;
+    var key = sessionStorage.getItem("id");
+    playsound('click');
+    if (!x) {
+        if (!key) {
+            if (element.id) {
+                key = element.id;
+            } else {
+                key = 1;
+            }
+        }
+    } else {
+        key = x;
+        document.getElementById("x").value = "";
+    }
+    increment(element, key);
+    getStage(parseInt(key));
+}
+
+function changeText(text) {
+    document.getElementById("outputDiv").innerHTML = text;
+}
+
+function playsound(audio) {
+    var snd = document.getElementById(audio);
+    snd.play();
+}
+
+function increment(element, key) {
+    var id = parseInt(key);
+    var newid = id + 1;
+
+    if (localStorage.getItem("len")) {
+        if (newid < localStorage.getItem("len")) {
+            if (element.id) {
+                element.id = newid;
+            } else {
+                element.id = key + 1;
+            }
+            sessionStorage.setItem("id", newid);
+        } else {
+            key = localStorage.getItem("len");
+        }
+    } else {
+        if (element.id) {
+            element.id = newid;
+        } else {
+            element.id = key + 1;
+        }
+        sessionStorage.setItem("id", newid);
+    }
+
+}
+
+function reset() {
+    document.getElementById(sessionStorage.getItem("id")).id = "";
+    sessionStorage.setItem("id", "");
+    sessionStorage.setItem("failed", "");
+    changeText("---");
+    document.getElementById('debug').innerHTML = 0;
+    localStorage.setItem("action", "");
+}
+
+function failed() {
+    playsound('wrong');
+    var failcount = parseInt(sessionStorage.getItem("failed"));
+    if (failcount) {
+        failcount += 1;
+    } else {
+        failcount = 1;
+    }
+    sessionStorage.setItem("failed", failcount);
+    if (failcount == 1) {
+        changeText("Nope, that's wrong!");
+    } else if (failcount == 2) {
+        changeText("Got you again!");
+    } else if (failcount == 3) {
+        changeText("Wow, 50% wrong so far!");
+    } else if (failcount == 4) {
+        changeText("You're actually really bad at this!");
+    } else if (failcount == 5) {
+        changeText("Are you messing with me?");
+    } else if (failcount > 5) {
+        changeText(". &nbsp;&nbsp;&nbsp; . &nbsp;&nbsp;&nbsp; .");
+    }
+
+}
+
+function readJson(stage) {
+    var i = parseInt(stage) - 1;
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+        "GET",
+        "https://raw.githubusercontent.com/Costigan-Stephen/CIT261-PT2/master/termwebsite/assets/stages.json",
+        true
+    );
+    xhr.send();
+
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var notify = JSON.parse(xhr.responseText);
+            var action = notify.stages[stage].action;
+            localStorage.setItem("action", action);
+            localStorage.setItem("len", notify.stages.length);
+
+            if (notify.stages[i].text.failed) {
+                if (sessionStorage.getItem("failed") >= 1) {
+                    changeText(notify.stages[i].text.failed);
+                } else {
+                    changeText(notify.stages[i].text.success);
+                }
+            } else {
+                changeText(notify.stages[i].text);
+            }
+            document.getElementById('stringify').innerHTML = JSON.stringify(notify.stages[i].action);
+        }
+    }
+}
---
+++ js/stages.js
@@ -0,0 +1,173 @@
+function getStage(stage) {
+    document.getElementById("debug").innerHTML = stage;
+
+    readJson(stage);
+    var action = localStorage.getItem("action");
+
+    switch (action) {
+        case 'none':
+            //Ensure no actions are taken by default
+            break;
+        case 'add.stage5': //5
+            document.getElementById("buttoncase").classList.add("stage5");
+            break;
+        case 'remove.stage5': //6
+            document.getElementById("buttoncase").classList.remove("stage5");
+            document.getElementById("buttoncase").classList.remove("stage5-active");
+            document.getElementById("buttoncase").classList.add("stage6");
+            break;
+        case 'remove.stage6': //7
+            document.getElementById("buttoncase").classList.remove("stage6");
+            break;
+        case 'multibutton.add': //17
+            multibutton("add", 5, "clone", 4);
+            break;
+        case 'multibutton.remove': //18
+            multibutton("remove", 0, "clone");
+            sessionStorage.setItem("failed", "");
+            break;
+        case 'opacity.add':
+            opacity('add');
+            break;
+        case 'opacity.fade':
+            opacity('fade');
+            break;
+        case 'opacity.remove':
+            opacity('remove');
+            break;
+        case 'water.add':
+            water('add');
+            break;
+        case 'water.drain':
+            water('fade');
+            break;
+        case 'water.remove':
+            water('remove');
+            break;
+        case 'draw.add':
+            draw('add');
+            break;
+        case 'draw.remove':
+            draw('remove');
+            break;
+        case 'colortest.add':
+            colortest('add');
+            break;
+        case 'colortest.fade':
+            colortest('fade');
+            break;
+        case 'colortest.remove':
+            colortest('remove');
+            sessionStorage.setItem("failed", "");
+            break;
+            // default:
+            //     changeText("Button Pressed " + stage + " times!");
+    }
+}
+
+function move(x) {
+    if (x.className == "frame stage5") {
+        x.className = "frame stage5-active";
+    } else if (x.className == "frame stage5-active") {
+        x.className = "frame stage5";
+    }
+}
+
+function multibutton(param, add, className, order) {
+    order--;
+    if (param == "add") {
+        var button = document.querySelector(".frame");
+        var buttonClone;
+        var innerbutton;
+        for (var x = 0; x < add; x++) {
+            buttonClone = button.cloneNode(true); //Clone the div and everything inside of it
+            buttonClone.classList.add(className); //Add class for identification/deletion
+
+            innerbutton = buttonClone.getElementsByClassName("button");
+            for (var i = 0; i < innerbutton.length; i++) {
+                innerbutton[i].setAttribute("onClick", "javascript: failed();");
+            }
+            if (x > order) {
+                document.querySelector("#outer").appendChild(buttonClone);
+            } else {
+                document.getElementById("outer").insertBefore(buttonClone, document.getElementById("outer").childNodes[0]);
+            }
+        }
+    } else {
+        var paras = document.getElementsByClassName(className);
+
+        while (paras[0]) {
+            paras[0].parentNode.removeChild(paras[0]);
+        }
+    }
+}
+
+function opacity(param) {
+    if (param == "add") {
+        document.querySelector(".frame").classList.add("opacity");
+    } else if (param == "fade") {
+        document.querySelector(".frame").classList.remove("opacity");
+        document.querySelector(".frame").classList.add("opacityfade");
+    } else {
+        document.querySelector(".frame").classList.remove("opacityfade");
+    }
+}
+
+function water(param) {
+    if (param == "add") {
+        var water = document.createElement("DIV");
+        var bubbles = document.createElement("DIV");
+        var button = document.querySelector(".frame");
+
+        button.classList.add("fullopacity");
+        buttonClone = button.cloneNode(true); //Clone the div and everything inside of it
+
+        water.classList.add("water");
+        water.id = "water";
+        bubbles.classList.add("bubbles");
+        water.appendChild(bubbles);
+        document.body.appendChild(water);
+
+    } else if (param == "fade") {
+        document.getElementById("buttoncase").classList.remove("fullopacity");
+        var paras = document.getElementById("water");
+
+        paras.classList.remove("water");
+        paras.classList.add("waterdrain");
+
+        var paras2 = document.getElementsByClassName("bubbles");
+        while (paras2[0]) {
+            paras2[0].parentNode.removeChild(paras2[0]);
+        }
+    } else {
+        var paras = document.getElementsByClassName("waterdrain");
+
+        while (paras[0]) {
+            paras[0].parentNode.removeChild(paras[0]);
+        }
+    }
+}
+
+function draw(param) {
+    var canvas = document.getElementById('canvas')
+    if (param == "add") {
+        var canvas = document.getElementById('canvas')
+        canvas.style.display = 'block';
+        canvas.style.visibility = 'visible';
+    } else {
+        canvas.style.display = 'none';
+        canvas.style.visibility = 'hidden';
+    }
+}
+
+function colortest(param) {
+    var canvas = document.getElementById('canvas')
+    if (param == "add") {
+        multibutton(param, 1, "colortest")
+    } else if (param == "fade") {
+        document.querySelector(".colortest").classList.remove("colortest");
+        document.querySelector(".colortest").style.visibility = hidden;
+    } else {
+        multibutton(param, "", "opacity")
+    }
+}
